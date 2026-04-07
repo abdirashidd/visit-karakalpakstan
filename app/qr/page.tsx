d8b8d0866f8e931ea3164d725d8f1f76fc9a5e9c
Abdirashid <shanlibaevabdirashid@gmail.com>
2026-04-07 06:09:28 +0500

    Fix: add default export to qr page

---
+++ app/qr/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+export default function QRPage() {
+  return (
+    <main style={{ minHeight: "100vh", background: "#0f172a", color: "white", padding: "40px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "32px", fontWeight: 800, color: "#38bdf8" }}>QR Skaner</h1>
+      <p style={{ marginTop: "20px", color: "#94a3b8" }}>Bu sahifa tayyorlanmoqda...</p>
+      <a href="/" style={{ color: "white", textDecoration: "underline", display: "block", marginTop: "30px" }}>Bosh sahifaga qaytish</a>
+    </main>
+  );
+}
